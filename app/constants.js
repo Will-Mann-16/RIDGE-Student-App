086fd87e9284500579089e269fbae3fb3909f8ba
Will Mann <william@mannclan.net>
2018-06-15 07:06:42 +0100

    Final Commit For App.

--- app/constants.js
+++ app/constants.js
@@ -5,19 +5,6 @@ export var INTERNAL_IP = "10.11.0.23";
 export var PORT = "8081";
 export var HOST_IP = "http://" + WELLY_IP + ":" + PORT;
 export var scriptsDirectory = HOST_IP + '/api/';
-export var axiosOpen = axios.create({
+export var instance = axios.create({
     baseURL: scriptsDirectory
-});
-axios.get("http://" + INTERNAL_IP + ":" + PORT + "/api/view-token").then((response) => {
-    HOST_IP = "http://" + INTERNAL_IP + ":" + PORT;
-    scriptsDirectory = HOST_IP + '/api/';
-    instance = axios.create({
-        baseURL: scriptsDirectory
-    });
-}).catch((error) =>{
-  HOST_IP = "http://" + WELLY_IP + ":" + PORT;
-  scriptsDirectory = HOST_IP + '/api/';
-  instance = axios.create({
-      baseURL: scriptsDirectory
-  });
 });
